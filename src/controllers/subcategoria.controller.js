e4b4d092100353d7600240111c35c4ac8f51efc8
JULIO DECIMA <JULIODECIMA@GMAIL.COM>
2021-06-14 15:03:36 -0300

    prueba

--- src/controllers/subcategoria.controller.js
+++ src/controllers/subcategoria.controller.js
@@ -30,28 +30,28 @@ export const getSubCategoriaByCategoria = async (req, res) => {
 };
 
 export const createSubCategoria = async (req, res) => {
-	try {
-		const foundUser = await Users.find({ tienda: { $in: req.tienda } });
-		const foundCategoria = await SubCategoria.find({ tienda: { $in: req.tienda } });
-		// const foundSubCategoria = await SubCategoria.find({
-		// 	categoria: { $in: req.body.categoria },
-		// });
-		const key = foundSubCategoria.length;
-		const { nombre, categoria } = req.body;
+	// try {
+	const foundUser = await Users.find({ tienda: { $in: req.tienda } });
+	const foundCategoria = await SubCategoria.find({ tienda: { $in: req.tienda } });
+	// const foundSubCategoria = await SubCategoria.find({
+	// 	categoria: { $in: req.body.categoria },
+	// });
+	const key = foundSubCategoria.length;
+	const { nombre, categoria } = req.body;
 
-		const newSubCategoria = new SubCategoria({
-			tienda: req.tienda,
-			nombre,
-			key: key ? key + 1 : 1,
-			categoria,
-			estado: true,
-		});
+	const newSubCategoria = new SubCategoria({
+		tienda: req.tienda,
+		nombre,
+		key: key ? key + 1 : 1,
+		categoria,
+		estado: true,
+	});
 
-		const savedSubCategoria = await newSubCategoria.save();
-		res.status(200).json(savedSubCategoria);
-	} catch (error) {
-		res.status(500).json({ mensaje: 'Error al ingresar subcategoria', error });
-	}
+	const savedSubCategoria = await newSubCategoria.save();
+	res.status(200).json(savedSubCategoria);
+	// } catch (error) {
+	// 	res.status(500).json({ mensaje: 'Error al ingresar subcategoria', error });
+	// }
 };
 
 export const updateSubCategoriaById = async (req, res) => {
